0be7d6de04824b955290a3a8d92abd7040471802
Alexsandr <artamoshkinas@gmail.com>
2024-03-27 09:05:30 +0700

    Добавлено: Создана модель Рецепт

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
+class Recipe(models.Model):
+    """Модель рецепта в БД"""
+    name = models.CharField(max_length=150)
+    discription = models.CharField(max_length=1000)
+    step = models.CharField(max_length=1500)
+    image = models.ImageField(upload_to='')
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='author_recipe')
+
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name}'
