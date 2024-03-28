d799ae16b8448c5500581202853f612a195eaf5f
Alexsandr <artamoshkinas@gmail.com>
2024-03-28 11:19:34 +0700

    Добавалено: Созданы модели таблиц Категория, связующая таблица рецепта и категории.

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from recipes.models import Recipe, Recipe_Category, Category
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'author', 'ingredient', 'created']
+
+
+@admin.register(Recipe_Category)
+class Recipe_CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created']
--- recipes/models.py
+++ recipes/models.py
@@ -7,11 +7,30 @@ class Recipe(models.Model):
     name = models.CharField(max_length=150)
     discription = models.CharField(max_length=1000)
     step = models.CharField(max_length=1500)
-    image = models.ImageField(upload_to='')
+    image = models.ImageField(upload_to='', blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               related_name='author_recipe')
+                               related_name='author')
+    ingredient = models.CharField(max_length=1000)
 
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.name}'
+
+
+class Category(models.Model):
+    """Модель категории в БД"""
+    name = models.CharField(max_length=150)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Recipe_Category(models.Model):
+    """Модель связующей таблицы рецептаи категорий в БД"""
+    recipe = models.ForeignKey('Recipe', on_delete=models.CASCADE, related_name='recipe')
+    catigory = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='category')
+
+    def __str__(self):
+        return f'{self.recipe}'
